6cba6d87966f0465b08516c98bb6339e649dc539
Tom <seriousbsnz@gmail.com>
2016-11-21 19:07:50 +0000

    update

--- server.js
+++ server.js
@@ -9,12 +9,23 @@ var http = require('http');  // This is how you require modules in node
 
 // server.listen(3000);
 
-http.createServer(function(request, response) {
-    response.writeHead(200); // Status code in header
-    response.write('Hello, this is me.');       // This is the callback in the event loop
-    response.end();
-}).listen(8080);
-console.log("Listening on 8080");
+
+
+// http.createServer(function(request, response) {
+//     response.writeHead(200); // Status code in header
+//     response.write('Hello, this is me.');       // This is the callback in the event loop
+//     response.end();
+// }).listen(8080);
+// console.log("Listening on 8080");
 
 // node server.js will then return listening on port 8080
 // in new terminal, curl http://localhost:8080 will return 'hello this is me'
+
+
+http.createServer(function(request, response) {
+    response.write('Dog is running');
+    setTimeout(function(){               //Will create new event that is called in 5 seconds
+        response.write('Dog is done');
+        response.end();
+    }, 5000);
+}).listen(8080);
